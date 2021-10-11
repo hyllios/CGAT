209e414f7ba429241c40ad649ca942965828f26b
JonathanSchmidt1 <schmidt.jonathan138@googlemail.com>
2021-10-11 10:27:16 +0200

    'Fixed prediction file for new lightning module'

--- CGAT/predict.py
+++ CGAT/predict.py
@@ -1,87 +1,42 @@
-from torch.utils.data import DataLoader
-from roost_message import collate_batch
-import pickle
-import gzip as gz
-from pytorch_lightning.callbacks import ModelCheckpoint
-import os
-from argparse import ArgumentParser
-import os
-import gc
-import datetime
-import numpy as np
-
-import numpy as np
-import torch
-import importlib
-import pytorch_lightning as pl
-from lightning_module import LightningTemplateModel
-from pytorch_lightning.loggers.tensorboard import TensorBoardLogger
-from torch_geometric.data import Batch
+from lightning_module import LightningModel
 from data import CompositionData
-from torch_geometric.data import DataListLoader
-import sys
-start = int(sys.argv[1])
-file = sys.argv[2]
+import torch
+from torch.utils.data import DataLoader
+import pickle, gzip as gz
 
-# path to model
-model = LightningTemplateModel.load_from_checkpoint('path.ckpt')
+
+model_path = '/home/test_user/QMC/test_CGAT/CGAT/CGAT/tb_logs/runs/f-{s}_t-11-10-2021_09:14:23/epoch=3-val_mae=0.19.ckpt'
+model = LightningModel.load_from_checkpoint(model_path, train=False)
+
+data_path = 'test_data/layered_perovskites_input.pickle.gz'
 dataset = CompositionData(
-    data_path=file,
+    data=data_path,
     fea_path=model.hparams.fea_path,
-    target_property=model.hparams.target_property,
-    max_neighbor_number=model.hparams.max_nbr,
-    start=start)
+    max_neighbor_number=model.hparams.max_nbr)
+
+
 params = {"batch_size": 5000,
           "pin_memory": False,
           "shuffle": False,
           "drop_last": False
           }
-print('length of train_subset', len(dataset))
-
 
 def collate_fn(datalist):
     return datalist
 
 
 model = model.cuda()
+dataloader = DataLoader(dataset, collate_fn=collate_fn, **params)
 
-model.eval()
-files = [] # add datasetfile names here
-datasets = []
-for file in files:
-    datasets.append(
-        CompositionData(
-            data_path=file,
-            fea_path=model.hparams.fea_path,
-            target_property=model.hparams.target_property,
-            max_neighbor_number=model.hparams.max_nbr,
-            start=0))
-
-prediction_list_big = []
-for dataset in datasets:
-    dataloader = DataLoader(dataset, collate_fn=collate_fn, **params)
-    prediction_list = []
-    model.eval()
-    for i, batch in enumerate(dataloader):
+prediction_list = []
+for i, batch in enumerate(dataloader):
+    with torch.no_grad():
         print(i)
-        with torch.no_grad():
-            device = next(model.model.parameters()).device
-            b_comp, batch = [el[1] for el in batch], [el[0] for el in batch]
-            batch = (Batch.from_data_list(batch)).to(device)
-            b_comp = collate_batch(b_comp)
-            b_comp = (tensor.to(device) for tensor in b_comp)
-            target = batch.y.view(len(batch.y), 1)
-            output, log_std = model(batch, b_comp).chunk(2, dim=1)
-            target_norm = model.norm(target)
-            prediction_list.append(
-                output * model.std.cuda() + model.mean.cuda())
-    prediction_list = torch.cat(prediction_list).flatten()
-    prediction_list_big.append(prediction_list)
+        prediction_list.append(model.evaluate(batch)[2]) # tuple of size 5 output, log_std, pred, target, target_norm
+
 
 pickle.dump(
-    torch.stack(prediction_list_big),
+    torch.cat(prediction_list),
     gz.open(
-        'predictions' +
-        file2 +
-        'pickle.gz',
+        'predictions.pickle.gz',
         'wb'))
