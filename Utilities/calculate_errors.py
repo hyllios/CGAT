faf6f575ca2f69bfebc002d19e79b11744ec80dc
noah-hoffmann <noahhoffmann1202@gmail.com>
2021-12-22 22:02:46 +0100

    adjusted data loading for calculating errors

--- Utilities/calculate_errors.py
+++ Utilities/calculate_errors.py
@@ -2,7 +2,7 @@ import gzip
 import pickle
 
 from CGAT.lightning_module import LightningModel, collate_fn
-from tqdm import tqdm
+from tqdm import trange
 from CGAT.data import CompositionData
 from torch.utils.data import DataLoader
 import pandas as pd
@@ -25,15 +25,15 @@ def main():
         required=True
     )
 
-    parser.add_argument(
-        '--idx',
-        type=int,
-        help='which data file to load: data_{idx*10000}_{(idx+1)*10000}.pickle.gz',
-        required=True
-    )
+    # parser.add_argument(
+    #     '--idx',
+    #     type=int,
+    #     help='which data file to load: data_{idx*10000}_{(idx+1)*10000}.pickle.gz',
+    #     required=True
+    # )
 
     hparams = parser.parse_args()
-    i = hparams.idx
+    # i = hparams.idx
     # Disable training for faster loading
     hparams.train = False
 
@@ -48,21 +48,22 @@ def main():
     errors = pd.DataFrame(columns=['batch_ids', 'errors'])
     PATH = 'active_learning'
     # iterate over unused data and evaluate the error
-    dataset = CompositionData(
-        data=get_file(i, PATH),
-        fea_path=hparams.fea_path,
-        max_neighbor_number=hparams.max_nbr,
-        target=hparams.target
-    )
-    data = pickle.load(gzip.open(get_file(i, PATH), 'rb'))
-    loader = DataLoader(dataset, batch_size=1, shuffle=False, collate_fn=collate_fn)
-    for j, batch in tqdm(enumerate(loader)):
-        _, _, pred, target, _ = model.evaluate(batch)
-        row = {'errors': mean_absolute_error(target.cpu().numpy(), pred.cpu().numpy()),
-               'batch_ids': data['batch_ids'][j][0]}
-        errors = errors.append(row, ignore_index=True)
+    for i in trange(283):
+        dataset = CompositionData(
+            data=get_file(i, PATH),
+            fea_path=hparams.fea_path,
+            max_neighbor_number=hparams.max_nbr,
+            target=hparams.target
+        )
+        data = pickle.load(gzip.open(get_file(i, PATH), 'rb'))
+        loader = DataLoader(dataset, batch_size=1, shuffle=False, collate_fn=collate_fn)
+        for j, batch in enumerate(loader):
+            _, _, pred, target, _ = model.evaluate(batch)
+            row = {'errors': mean_absolute_error(target.cpu().numpy(), pred.cpu().numpy()),
+                   'batch_ids': data['batch_ids'][j][0]}
+            errors = errors.append(row, ignore_index=True)
 
-    errors.to_csv(get_file(i, PATH + '/temp').replace('data', 'errors').replace('pickle.gz', 'csv'))
+        errors.to_csv(get_file(i, PATH + '/temp').replace('data', 'errors').replace('pickle.gz', 'csv'))
 
 
 if __name__ == '__main__':
