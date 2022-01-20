9299c3b0135de78639aaf16eac7948ffe8192d81
JonathanSchmidt1 <schmidt.jonathan138@googlemail.com>
2022-01-20 11:27:16 +0100

    changed CGAT for loading old models with newer pl version and changed predict.py for multi gpu (requires changes in pytorch lightning predict loop)

--- CGAT/predict.py
+++ CGAT/predict.py
@@ -3,22 +3,21 @@ from data import CompositionData
 import torch
 from torch.utils.data import DataLoader
 import pickle, gzip as gz
-
-#example script for predicting data with a trained model
-
-
-model_path = 'example.ckpt'
+from pytorch_lightning import Trainer
+torch.set_printoptions(6)
+#model_path = 'tb_logs/runs/f-{s}_t-30-11-2021_12:27:59/epoch=261-val_mae=0.24.ckpt'
+model_path = 'tb_logs/runs/f-{s}_t-25-11-2021_21:10:33/epoch=261-val_mae=0.02.ckpt'
 model = LightningModel.load_from_checkpoint(model_path, train=False)
-#set train=False to avoid reloading of large training datasets
 
-data_path = 'example_test_data.pickle.gz'
+#data_path = 'test_all_same_new.pickle.gz'
+data_path = '../../data_noah/data_2220000_2230000.pickle.gz'
 dataset = CompositionData(
     data=data_path,
-    fea_path=model.hparams.fea_path,
-    max_neighbor_number=model.hparams.max_nbr)
+    fea_path=model.hparams.hparams["fea_path"],
+    max_neighbor_number=model.hparams.hparams["max_nbr"], target='e_above_hull_new')
+print(len(dataset))
 
-
-params = {"batch_size": 5000,
+params = {"batch_size": 234,
           "pin_memory": False,
           "shuffle": False,
           "drop_last": False
@@ -31,12 +30,11 @@ def collate_fn(datalist):
 model = model.cuda()
 dataloader = DataLoader(dataset, collate_fn=collate_fn, **params)
 
-prediction_list = []
-for i, batch in enumerate(dataloader):
-    with torch.no_grad():
-        print(i)
-        prediction_list.append(model.evaluate(batch)[2]) # tuple of size 5 output, log_std, pred, target, target_norm
 
-        
-pickle.dump(torch.cat(prediction_list),gz.open('predictions.pickle.gz', 'wb'))
+trainer = Trainer(gpus=10, strategy='ddp')
+prediction_list = trainer.predict(model, dataloader)
+
+#print(torch.mean(torch.cat(prediction_list)))
+print(prediction_list)        
+pickle.dump(prediction_list,gz.open('new_predict.pickle.gz','wb'))
 
