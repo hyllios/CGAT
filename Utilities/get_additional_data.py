c423c708020da9dc7652d6588baebad1461da3c8
noah-hoffmann <noahhoffmann1202@gmail.com>
2022-02-10 12:08:48 +0100

    fix of get_additional_data.py

--- Utilities/get_additional_data.py
+++ Utilities/get_additional_data.py
@@ -6,25 +6,27 @@ import bz2
 import gzip as gz
 import json
 from pymatgen.entries.computed_entries import ComputedEntry
+from tqdm import tqdm
 
 
 def get_composition(file: str):
     pattern = re.compile(r'/(A\d+B\d+C\d+)/')
-    return pattern.match(file)[1]
+    return pattern.search(file)[1]
 
 
 def get_file_name(file: str):
     pattern = re.compile(r'/([\w-]*)\.json\.bz2')
-    return pattern.match(file)[1]
+    return pattern.search(file)[1]
 
 
 def main():
     PATH = "/nfs/data-019/marques/data/material_prediction_CGAT/ternaries"
     files = glob.glob(os.path.join(PATH, '*', 'annotated', '*.json.bz2'))
+    print(f"Found {len(files)} files.")
     new_dir = "additional_data"
     if not os.path.exists(new_dir):
         os.mkdir(new_dir)
-    for file in files:
+    for file in tqdm(files):
         dir = os.path.join(new_dir, get_composition(file))
         if not os.path.exists(dir):
             os.mkdir(dir)
