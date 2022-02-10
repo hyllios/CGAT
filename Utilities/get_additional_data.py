d7404ef6014f48e7f0da5a9e0d047ee0dbc80ab9
noah-hoffmann <noahhoffmann1202@gmail.com>
2022-02-10 11:48:43 +0100

    first version of adding additional data

--- Utilities/get_additional_data.py
+++ Utilities/get_additional_data.py
@@ -1,10 +1,36 @@
 import os
 import glob
+import pickle
+import re
+import bz2
+import gzip as gz
+import json
+from pymatgen.entries.computed_entries import ComputedEntry
+
+
+def get_composition(file: str):
+    pattern = re.compile(r'/(A\d+B\d+C\d+)/')
+    return pattern.match(file)[1]
+
+
+def get_file_name(file: str):
+    pattern = re.compile(r'/([\w-]*)\.json\.bz2')
+    return pattern.match(file)[1]
 
 
 def main():
-    PATH = "~marques/data/material_prediction_CGAT/ternaries"
-    print(glob.glob(os.path.join(PATH, '*/annotated/*.json.bz2')))
+    PATH = "/nfs/data-019/marques/data/material_prediction_CGAT/ternaries"
+    files = glob.glob(os.path.join(PATH, '*', 'annotated', '*.json.bz2'))
+    new_dir = "additional_data"
+    if not os.path.exists(new_dir):
+        os.mkdir(new_dir)
+    for file in files:
+        dir = os.path.join(new_dir, get_composition(file))
+        if not os.path.exists(dir):
+            os.mkdir(dir)
+        json_data = json.load(bz2.open(file, 'rb'))
+        data = list(map(ComputedEntry.from_dict, json_data['entries']))
+        pickle.dump(data, gz.open(os.path.join(dir), f'unprepared-{get_file_name(file)}.pickle.gz'))
 
 
 if __name__ == '__main__':
