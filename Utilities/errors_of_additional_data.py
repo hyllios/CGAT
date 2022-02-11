be53f38133a5980d7baf10830d4ce85acccbcb82
noah-hoffmann <noahhoffmann1202@gmail.com>
2022-02-11 13:01:03 +0100

    fixed get_composition and refined error calculation

--- Utilities/errors_of_additional_data.py
+++ Utilities/errors_of_additional_data.py
@@ -7,32 +7,43 @@ from sklearn.metrics import mean_absolute_error
 import glob
 from get_additional_data import get_composition
 from tqdm import tqdm
+import numpy as np
 
 
 def main():
     data_paths = glob.glob(os.path.join("additional_data", "*", "*.pickle.gz"))
     assert len(data_paths) > 0
     print(f"Found {len(data_paths)} datasets")
-    model_path = glob.glob(os.path.join("tb_logs",
-                                        "runs",
-                                        "f-0_t-2022-02-02_12-39-57",
-                                        "*.ckpt"))[0]
-    model = LightningModel.load(model_path)
-    df = pd.DataFrame(columns=['comp', 'mae'])
+    sizes = [50_000, 75_000, 100_000, 125_000, 150_000, 200_000, 250_000]
+    runs = ["f-0_t-2022-01-03_15-07-47",
+            "f-0_t-2022-01-09_14-33-04",
+            "f-0_t-2022-01-14_14-50-14",
+            "f-0_t-2022-01-18_10-00-20",
+            "f-0_t-2022-01-22_17-40-56",
+            "f-0_t-2022-01-26_22-42-02",
+            "f-0_t-2022-02-02_12-39-57"]
+    model_paths = [glob.glob(os.path.join("tb_logs",
+                                          "runs",
+                                          "{run}",
+                                          "*.ckpt").format(run=run))[0] for run in runs]
+    df = pd.DataFrame(columns=['comp', 'training set size', 'mae'])
+    for model_path, size in zip(tqdm(model_paths), sizes):
+        model = LightningModel.load(model_path)
 
-    for path in tqdm(data_paths):
-        dataset = CompositionData(
-            data=path,
-            fea_path="embeddings/matscholar-embedding.json",
-            max_neighbor_number=model.hparams.max_nbr,
-            target=model.hparams.target
-        )
-        loader = DataLoader(dataset, batch_size=len(dataset), shuffle=False, collate_fn=collate_fn)
-        comp = get_composition(path)
-        for batch in loader:
-            _, _, pred, target, _ = model.evaluate(batch)
-            error = mean_absolute_error(target.cpu().numpy(), target.cpu().numpy())
-            df.loc[len(df)] = [comp, error]
+        for path in tqdm(data_paths):
+            dataset = CompositionData(
+                data=path,
+                fea_path="embeddings/matscholar-embedding.json",
+                max_neighbor_number=model.hparams.max_nbr,
+                target=model.hparams.target
+            )
+            loader = DataLoader(dataset, batch_size=1, shuffle=False, collate_fn=collate_fn)
+            comp = get_composition(path)
+            errors = []
+            for batch in loader:
+                _, _, pred, target, _ = model.evaluate(batch)
+                errors.append(mean_absolute_error(target.cpu().numpy(), pred.cpu().numpy()))
+            df.loc[len(df)] = [comp, size, np.mean(errors)]
     df.to_csv('additional_data/errors.csv', index=False)
 
 
--- Utilities/get_additional_data.py
+++ Utilities/get_additional_data.py
@@ -10,8 +10,8 @@ from CGAT.prepare_volume_data import build_dataset_prepare
 
 
 def get_composition(file: str):
-    pattern = re.compile(r'[A-Z]\d+')
-    return "".join(pattern.findall(file))
+    pattern = re.compile(r'(?:/|\\)' + r'([A-Z]\d*)' + r'([A-Z]\d*)?' * 10 + r'(?:/|\\)')
+    return "".join(filter(None, pattern.search(file).groups()))
 
 
 def get_file_name(file: str):
@@ -38,5 +38,34 @@ def main():
             pickle.dump(build_dataset_prepare(data, target_property=['e_above_hull_new', 'e-form']), f)
 
 
+def test_get_composition():
+    cases = [
+        "/nfs/data-019/marques/data/material_prediction_CGAT/binaries/A2B13/annotated/batch-000.json.bz2",
+        "/nfs/data-019/marques/data/material_prediction_CGAT/binaries/A2B3/annotated/batch-000.json.bz2",
+        "/nfs/data-019/marques/data/material_prediction_CGAT/binaries/AB12/annotated/batch-000.json.bz2",
+        "/nfs/data-019/marques/data/material_prediction_CGAT/binaries/AB2/annotated/batch-000.json.bz2",
+        "/nfs/data-019/marques/data/material_prediction_CGAT/binaries/AB/annotated/batch-000.json.bz2",
+        "/nfs/data-019/marques/data/material_prediction_CGAT/ternaries/A2B2C5/annotated/batch-000.json.bz2",
+        "/nfs/data-019/marques/data/material_prediction_CGAT/ternaries/A3B4C12/annotated/batch-000.json.bz2",
+    ]
+    results = [
+        "A2B13",
+        "A2B3",
+        "AB12",
+        "AB2",
+        "AB",
+        "A2B2C5",
+        "A3B4C12"
+    ]
+    for case, result in zip(cases, results):
+        try:
+            assert get_composition(case) == result
+        except AssertionError:
+            print("Fails for: ", repr(case))
+            print("Expected: ", repr(result))
+            print("Found:", repr(get_composition(case)))
+
+
 if __name__ == '__main__':
-    main()
+    test_get_composition()
+    # main()
