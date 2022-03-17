9532d44fa77774831105f0178bd08e8b2ba030c0
noah-hoffmann <noahhoffmann1202@gmail.com>
2022-03-17 13:06:08 +0100

    script for preparing 'real' active learning run

---
+++ Utilities/prepare_active_learning.py
@@ -0,0 +1,44 @@
+import pickle
+import gzip as gz
+import os
+from tqdm import tqdm
+from glob import glob
+from adjust_data import remove_batch_ids
+
+
+def load(path):
+    return pickle.load(gz.open(path))
+
+
+def save(data, path):
+    pickle.dump(data, gz.open(path, 'wb'))
+
+
+def main():
+    data_dir = 'data'
+    used_data_path = os.path.join('active_learning', 'sample', 'randoms', 'random_sample_150000.pickle.gz')
+    target_dir = os.path.join('new_active_learning', 'remaining')
+
+    if not os.path.isdir(target_dir):
+        os.makedirs(target_dir)
+
+    test_val_data = load(os.path.join(data_dir, 'indices', 'test_and_val_idx.pickle.gz'))
+    test_ids = set([batch_id for batch_id, in test_val_data['test_batch_ids']])
+    val_ids = set([batch_id for batch_id in test_val_data['val_batch_ids']])
+
+    used_data = load(used_data_path)
+    used_ids = set([batch_id for batch_id, in used_data['batch_ids']])
+
+    ids = test_ids | val_ids | used_ids
+
+    for file in tqdm(glob(os.path.join(data_dir, '*.pickle.gz'))):
+        data = load(file)
+        to_remove = set()
+
+        for batch_id, in data['batch_ids']:
+            if batch_id in ids:
+                to_remove.add(batch_id)
+                ids.remove(batch_id)
+
+        remove_batch_ids(data, to_remove)
+        save(data, os.path.join(target_dir, os.path.basename(file)))
