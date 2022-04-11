6b1d52b7e22f4dfde36955ae7d99edea9b54bcd3
noah-hoffmann <noahhoffmann1202@gmail.com>
2022-04-11 17:00:46 +0200

    aktueller Stand

--- Utilities/tsne.py
+++ Utilities/tsne.py
@@ -3,8 +3,14 @@ from pathlib import Path
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.metrics import mean_absolute_error
-from fastTSNE import TSNE
+from openTSNE import TSNE
 from tqdm import tqdm
+
+
+def cap(array, upper_limit):
+    temp = np.full((len(array), 2), upper_limit)
+    temp[:, 0] = array
+    return np.amin(temp, axis=1)
 
 
 def main():
@@ -12,12 +18,18 @@ def main():
 
     embeddings = []
     errors = []
-    for comp in tqdm(list(comps)):
+    colors = []
+    targets = []
+    comp_id = []
+
+    cycle = plt.rcParams['axes.prop_cycle'].by_key()['color']
+
+    for i, comp in enumerate(tqdm(list(comps))):
         files = comp.glob('*.txt')
         predictions = pd.DataFrame()
         for file in files:
             if 'target' == file.stem:
-                targets = np.loadtxt(file)
+                targets.append(np.loadtxt(file))
                 # targets.append(target)
             elif 'log_std' in file.stem:
                 pass
@@ -28,17 +40,34 @@ def main():
                     predictions[int(file.stem)] = np.loadtxt(file)
                 except ValueError:
                     pass
-        errors.append(np.array([mean_absolute_error([targets[i]] * len(row.keys()), row) for i, row in
+        errors.append(np.array([mean_absolute_error([targets[-1][i]] * len(row.keys()), row) for i, row in
                                 predictions.iterrows()]
                                ))
+        colors += [cycle[i % len(cycle)]] * len(errors[-1])
+        comp_id += [i] * len(errors[-1])
 
     embeddings = np.concatenate(embeddings)
     errors = np.concatenate(errors)
+    targets = np.concatenate(targets)
 
-    print(embeddings.shape)
-    tsne = TSNE()
+    errors = cap(errors, .4)
+    targets = cap(targets, .5)
+
+    tsne = TSNE(n_jobs=6, perplexity=300)
     embedding = tsne.fit(embeddings)
-    print(embedding.shape)
+
+    plt.title('Error')
+    plt.scatter(embedding[:, 0], embedding[:, 1], c=errors, s=.5)
+    plt.colorbar()
+    plt.figure()
+    plt.title('Prototypes')
+    plt.scatter(embedding[:, 0], embedding[:, 1], c=colors, s=.5)
+    plt.show()
+    plt.figure()
+    plt.title('distance to convex hull')
+    plt.scatter(embedding[:, 0], embedding[:, 1], c=targets, s=.5)
+    plt.colorbar()
+    plt.show()
 
 
 if __name__ == '__main__':
