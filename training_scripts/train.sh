834408a94f15661d37a9a96754fee43b6d3f5acf
JonathanSchmidt1 <schmidt.jonathan138@googlemail.com>
2023-01-09 17:49:06 +0100

    simplified training scripts

--- training_scripts/train.sh
+++ training_scripts/train.sh
@@ -1,21 +1,8 @@
-embedding_path="embeddings/matscholar-embeddings.json"
-for directory in "prepared" "prepared/val" "prepared/test"
-do
-  if [ ! -d "$directory" ]
-  then
-    echo "Can't find directory '$directory', please check current working directory!"
-    exit 1
-  fi
-done
-if [ ! -f "$embedding_path" ]
-then
-  echo "Can't find embedding file, please check current working directory!"
-  exit 1
-fi
+embedding_path="embeddings/matscholar-embedding.json"
 
 # Training loop
-for target in e_above_hull_new e-form volume
+for target in e_above_hull
 do
   echo Training target "$target"
-  train-CGAT --gpus 12 --target "$target" --fea-path "$embedding_path" --epochs 280 --clr-period 70 --data-path prepared --val-path prepared/val --test-path prepared/test
+  train-CGAT --gpus 2 --target "$target" --fea-path "$embedding_path" --epochs 280 --clr-period 70 --data-path ../test_data_github/dcgat_features.pickle.gz --batch-size 2
 done
