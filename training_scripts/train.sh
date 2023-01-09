c1e2b909c7ba5a66054bda10b1f5a3a23c5d8f34
JonathanSchmidt1 <54937397+JonathanSchmidt1@users.noreply.github.com>
2023-01-09 18:36:52 +0100

    Update train.sh

--- training_scripts/train.sh
+++ training_scripts/train.sh
@@ -4,5 +4,5 @@ embedding_path="embeddings/matscholar-embedding.json"
 for target in e_above_hull
 do
   echo Training target "$target"
-  train-CGAT --gpus 2 --target "$target" --fea-path "$embedding_path" --epochs 280 --clr-period 70 --data-path ../test_data_github/dcgat_features.pickle.gz --batch-size 2
+  train-CGAT --gpus 2 --target "$target" --fea-path "$embedding_path" --epochs 280 --clr-period 70 --data-path dcgat_1_000_features.pickle.gz --batch-size 2
 done
